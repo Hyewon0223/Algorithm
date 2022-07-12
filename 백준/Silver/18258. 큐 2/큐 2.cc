86fcbaf9993ac6c58b9c5692be6ea67641d80c8b
최혜원 <54919662+Hyewon0223@users.noreply.github.com>
2022-07-12 20:37:49 +0900

    [Silver IV] Title: 큐 2, Time: 404 ms, Memory: 8416 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,41 @@
+// Hyewon Choi
+// 2022.07.12.TUE
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int N, X;
+    string command;
+    queue<int> queue;
+    cin >> N;
+    for (int i=0;i<N;i++){
+        cin >> command;
+        if (command == "push") {
+            cin >> X;
+            queue.push(X);
+        }
+        else if (command == "pop") {
+            if (!queue.empty()) {
+                cout << queue.front() << '\n';
+                queue.pop();
+            }
+            else cout <<  -1 << '\n';
+        }
+        else if (command == "size") cout << queue.size() << '\n';
+        else if (command == "empty") {
+             if (queue.empty()) cout << 1 << '\n';
+             else cout << 0 << '\n';
+        }
+        else if (command == "front" || command == "back") {
+            if (!queue.empty()) {
+                if (command == "front") cout << queue.front() << '\n';
+                else cout << queue.back() << '\n'; // command == "back"
+            }
+            else cout << -1 << '\n';
+        }
+    }
+}
