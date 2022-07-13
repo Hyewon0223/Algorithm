a3393bf0726088da5f792714104b7fa241713218
최혜원 <54919662+Hyewon0223@users.noreply.github.com>
2022-07-13 20:11:05 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,20 @@
+// Hyewon Choi
+// 2022.07.13.WED
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    queue<int> q; 
+    for (int i=0;i<N;i++) q.push(i+1);
+
+    while(q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front() << "\n";
+}
